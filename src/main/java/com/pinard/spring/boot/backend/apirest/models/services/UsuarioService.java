00b07f15e0164f411ecfccbdc1358123b3b2ca99
teseiramartin <teseiramartin@gmail.com>
2020-08-25 19:57:23 -0300

    spring security

--- src/main/java/com/pinard/spring/boot/backend/apirest/models/services/UsuarioService.java
+++ src/main/java/com/pinard/spring/boot/backend/apirest/models/services/UsuarioService.java
@@ -3,6 +3,8 @@ package com.pinard.spring.boot.backend.apirest.models.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,6 +20,8 @@ import com.pinard.spring.boot.backend.apirest.models.entity.Usuario;
 
 @Service
 public class UsuarioService implements UserDetailsService{
+	
+	private Logger logger = LoggerFactory.getLogger(UsuarioService.class);
 
 	@Autowired
 	private IUsuarioDao usuarioDao;
@@ -28,9 +32,15 @@ public class UsuarioService implements UserDetailsService{
 		
 		Usuario usuario = this.usuarioDao.findByUsername(username);
 		
+		if (usuario == null) {
+			logger.error("Error en el login: no existe el usuario '"+username+"' en el sistema!");
+			throw new UsernameNotFoundException("Error en el login: no existe el usuario '"+username+"' en el sistema!");
+		}
+		
 		List<GrantedAuthority> authorities = usuario.getRoles()
 				.stream()
 				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+				.peek(authority -> logger.info("Role: " + authority.getAuthority()))
 				.collect(Collectors.toList());
 		
 		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
