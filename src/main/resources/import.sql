856080712adf8706f9dccb471b5f9741ce54c379
teseiramartin <teseiramartin@gmail.com>
2020-08-27 15:47:21 -0300

    spring security

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -20,3 +20,15 @@ INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, '
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(5, 'Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(6, 'Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(7, 'Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
+
+/* Creamos algunos usuarios con sus roles */
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$JK08YqRKq4nLw2UhEEjf0O8FJIJQ2jH4Baeby6X4fK7767adAsn4a',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$eKtmj0sj5aluJXeoJ/MYdOtD.xg9U5hWN7/4YXcKkQCHSek5R4IEi',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+
